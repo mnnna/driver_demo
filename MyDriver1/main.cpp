50483233b0fbe4716c8de0c6aad28d5658548a11
zhengyuhang <zhengyuhang@x-cmd.com>
2024-02-24 02:47:03 +0800

    update

--- MyDriver1/main.cpp
+++ MyDriver1/main.cpp
@@ -1,5 +1,25 @@
 #include<ntifs.h>
 #include<ntddk.h>
+#include<intrin.h>
+#include"ia32/ia32.hpp" 
+
+#pragma warning(disable:4389)
+void* GetPteBase() {
+	cr3 CR3;
+	PHYSICAL_ADDRESS cr3_pa = { 0 }; 
+	CR3.flags =  __readcr3();
+	cr3_pa.QuadPart = CR3.address_of_page_directory * PAGE_SIZE; 
+	PULONG64 cr3_va  = (PULONG64) MmGetVirtualForPhysical(cr3_pa);
+
+	UINT64 nCount = 0;
+	while ((*cr3_va & 0x000FFFFFFFFFF000) != cr3_pa.QuadPart) {
+		if (++nCount >= 512) {
+			return nullptr;
+		}
+		cr3_va++;
+	} 
+	return (void*)(0xffff000000000000 | (nCount << 39));
+}
 
 void DriverUnload(PDRIVER_OBJECT DriverObject) {
 	DriverObject;
@@ -8,7 +28,9 @@ void DriverUnload(PDRIVER_OBJECT DriverObject) {
 EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PLSA_UNICODE_STRING RegistryPath) {
 	DbgPrint("DriverObject:%p\n", DriverObject);
 	DbgPrint("RegistryPath :%wz\n", RegistryPath);
-	DbgBreakPoint();
+	 DbgBreakPoint();
+	PVOID64 PteBase = GetPteBase();
+	DbgPrint("PteBase :%p\n", PteBase);
 	DriverObject->DriverUnload = DriverUnload;
 	return  STATUS_SUCCESS;
 } 
