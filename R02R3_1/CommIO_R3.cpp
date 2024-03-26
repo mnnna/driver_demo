609dc9a2ba3b59b3dec4a1f1c42b788e4be27923
zhengyuhang <zhengyuhang@x-cmd.com>
2024-03-27 01:57:36 +0800

    update

--- R02R3_1/CommIO_R3.cpp
+++ R02R3_1/CommIO_R3.cpp
@@ -2,28 +2,28 @@
 #include <iostream>
 #include <winioctl.h>
 
-#define DEVICE_LINK_NAME "\\\\.\\DemoInject"
+#define DEVICE_LINK_NAME "\\\\.\\DemoInject" // R3 format  
 
 void main(){
 	BOOL bRet; 
-	HANDLE hDevice = NULL;
+	HANDLE hDevice = NULL; 
 
-	hDevice = CreateFileA(DEVICE_LINK_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, 0, NULL, NULL);
+	hDevice = CreateFileA(DEVICE_LINK_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, NULL, NULL);
 
 	if (hDevice == (HANDLE)-1) {  //在 Windows 编程中，通常使用 -1 来表示一个特殊的无效句柄（INVALID_HANDLE_VALUE）
 		printf("Create file faild ! \n ");
 	}
 
-	char outBuff[MAXBYTE] = { 0 };
-	char inBuff[MAXBYTE] = "Form R3";
+	//char outBuff[MAXBYTE] = { 0 };
+	//char inBuff[MAXBYTE] = "Form R3";
 
-	bRet = ReadFile(hDevice, outBuff, sizeof(outBuff), NULL, NULL);
-	if (!bRet) {
-		printf("Read file failed\n");
-	}
-	else {
-		printf("Read file outBuff: %s \n", outBuff);
-	}
+	//bRet = ReadFile(hDevice, outBuff, sizeof(outBuff), NULL, NULL);
+	//if (!bRet) {
+	//	printf("Read file failed\n");
+	//}
+	//else {
+	//	printf("Read file outBuff: %s \n", outBuff);
+	//}
 
 	/*bRet = WriteFile(hDevice, inBuff, sizeof(inBuff), NULL, NULL);
 	if (!bRet) {
--- R02R3_1_r0/Drivermain.cpp
+++ R02R3_1_r0/Drivermain.cpp
@@ -38,50 +38,53 @@ NTSTATUS DispatchCreate(PDEVICE_OBJECT DeviceObject , PIRP pIrp) {
 
 }
 
-NTSTATUS DispatchRead(PDEVICE_OBJECT DeviceObject, PIRP pIrp) {
-	UNREFERENCED_PARAMETER(DeviceObject);
+//NTSTATUS DispatchRead(PDEVICE_OBJECT DeviceObject, PIRP pIrp) {
+//	UNREFERENCED_PARAMETER(DeviceObject);
+//
+//	char buff[255] = "hello world r0";
+//
+//	PVOID sysBuff = pIrp->AssociatedIrp.SystemBuffer;
+//	if (MmIsAddressValid(sysBuff)) {
+//		DbgPrint("sysBuff is null！\n");
+//		pIrp->IoStatus.Information = 0;
+//		pIrp->IoStatus.Status = STATUS_SUCCESS;
+//		IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+//		return STATUS_SUCCESS;
+//	}
+//	RtlCopyMemory(sysBuff, buff, sizeof(buff));
+//
+// }
 
-	char buff[255] = "hello world r0";
-
-	PVOID sysBuff = pIrp->AssociatedIrp.SystemBuffer;
-	if (MmIsAddressValid(sysBuff)) {
-		DbgPrint("sysBuff is null！\n");
-		pIrp->IoStatus.Information = 0;
-		pIrp->IoStatus.Status = STATUS_SUCCESS;
-		IoCompleteRequest(pIrp, IO_NO_INCREMENT);
-		return STATUS_SUCCESS;
-	}
-	RtlCopyMemory(sysBuff, buff, sizeof(buff));
-
-}
 
 
-
-NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegisterPath) {
+EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegisterPath) {
 	RegisterPath;
 	DriverObject->DriverUnload = DriverUnload;
 
 	NTSTATUS status;
 	PDEVICE_OBJECT PDeviceObj;
 	UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\Device\\DemoInject");
-	UNICODE_STRING symbolLINKName = RTL_CONSTANT_STRING(L"\\ ? ? \\DemoInject");
+	UNICODE_STRING symbolLINKName = RTL_CONSTANT_STRING(L"\\??\\DemoInject");
 
 	//创建设备对象
 	status = IoCreateDevice(DriverObject, NULL, &DeviceName, FILE_DEVICE_UNKNOWN, NULL, FALSE, &PDeviceObj);
-
-	if (NT_SUCCESS(status)){
+	if (!NT_SUCCESS(status)){
 		return status ;
 	}
-	PDeviceObj->Flags = 0; // 将 PDeviceObj->Flags 设置为 0，意味着清除所有的标志位，将设备对象恢复到默认状态。
-
+	
+	//PDeviceObj->Flags = 0; // 将 PDeviceObj->Flags 设置为 0，意味着清除所有的标志位，将设备对象恢复到默认状态。
 
 	//创建 R0 与 R3 的符号链接 
 	status = IoCreateSymbolicLink(&symbolLINKName, &DeviceName);
-	if (NT_SUCCESS(status)) {
+	if (!NT_SUCCESS(status)) {
 		return status;
 	}
 	//例程
+	DbgPrint("驱动加载成功！\n");
 	DriverObject->MajorFunction[IRP_MJ_CREATE] = DispatchCreate;
+	DbgPrint("驱动加载成功！\n");
 	DriverObject->MajorFunction[IRP_MJ_CLOSE] = DispatchClose;
-	DriverObject->MajorFunction[IRP_MJ_READ] = DispatchRead;
+	//DriverObject->MajorFunction[IRP_MJ_READ] = DispatchRead;
+	DbgPrint("驱动加载成功！\n");
+	return STATUS_SUCCESS;
 }
