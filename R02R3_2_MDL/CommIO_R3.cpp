36a01b6a56377f4b0895b5f89d7ef70dd67d8ac2
zhengyuhang <zhengyuhang@x-cmd.com>
2024-03-28 01:25:15 +0800

    update

---
+++ R02R3_2_MDL/CommIO_R3.cpp
@@ -0,0 +1,43 @@
+#include<Windows.h>
+#include <iostream>
+#include <winioctl.h>
+
+#define DEVICE_LINK_NAME "\\\\.\\DemoInject" // R3 format  
+
+void main(){
+	BOOL bRet; 
+	HANDLE hDevice = NULL; 
+
+	hDevice = CreateFileA(DEVICE_LINK_NAME, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, NULL, NULL);
+
+	if (hDevice == (HANDLE)-1) {  //在 Windows 编程中，通常使用 -1 来表示一个特殊的无效句柄（INVALID_HANDLE_VALUE）
+		printf("Create file faild ! \n ");
+	}
+
+	char outBuff[MAXBYTE] = { 0 };
+	char inBuff[MAXBYTE] = "Hello world from R3";
+
+	bRet = ReadFile(hDevice, outBuff, sizeof(outBuff), NULL, NULL);
+	if (!bRet) {
+		printf("Read file failed\n");
+	}
+	else {
+		printf("Read file outBuff: %s \n", outBuff);
+	}
+	 
+	bRet = WriteFile(hDevice, inBuff, sizeof(inBuff), NULL, NULL);
+	if (!bRet) {
+		printf("write file failed\n");
+	}
+	else {
+		printf("write file outBuff: %s \n", inBuff);
+	}
+
+
+	system("pause");
+
+	CloseHandle(hDevice);
+
+	system("pause");
+
+}
---
+++ R02R3_2_r0_MDL/Drivermain.cpp
@@ -0,0 +1,116 @@
+#include<ntifs.h>
+#include<ntddk.h>
+
+void DriverUnload(PDRIVER_OBJECT DriverObject) {
+
+	if (DriverObject->DeviceObject) {
+		IoDeleteDevice(DriverObject->DeviceObject);
+		UNICODE_STRING symbolLinkName = RTL_CONSTANT_STRING(L"\\??\\DemoInject");
+		NTSTATUS status = IoDeleteSymbolicLink(&symbolLinkName);
+		if (!NT_SUCCESS(status)) {
+			DbgPrint("符号删除成功！\n");
+		}
+	}
+	DbgPrint("驱动卸载成功！\n");
+}
+
+NTSTATUS DispatchClose(PDEVICE_OBJECT DeviceObject, PIRP pIrp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+	//返回给3ring
+	pIrp->IoStatus.Information = 0;
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+
+	DbgPrint("DispatchCreate！\n");
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+
+}
+
+NTSTATUS DispatchCreate(PDEVICE_OBJECT DeviceObject , PIRP pIrp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+	//返回给3ring
+	pIrp->IoStatus.Information = 0;
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+
+	DbgPrint("DispatchCreate！\n");
+	IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+
+}
+
+NTSTATUS DispatchRead(PDEVICE_OBJECT DeviceObject, PIRP pIrp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+
+	char buff[255] = "hello world form r0 \n";
+
+	PVOID sysBuff = pIrp->AssociatedIrp.SystemBuffer;
+	if (!MmIsAddressValid(sysBuff)) {
+		DbgPrint("sysBuff is null！\n");
+		pIrp->IoStatus.Information = 0;
+		pIrp->IoStatus.Status = STATUS_SUCCESS;
+		IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+		return STATUS_SUCCESS;
+	}
+	RtlCopyMemory(sysBuff, buff, sizeof(buff));
+	pIrp->IoStatus.Information = sizeof(buff);
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	return STATUS_SUCCESS;
+
+ }
+
+NTSTATUS DispatchWrite(PDEVICE_OBJECT DeviceObject, PIRP pIrp) {
+	UNREFERENCED_PARAMETER(DeviceObject);
+	 
+	auto stack = IoGetCurrentIrpStackLocation(pIrp);
+	stack->Parameters.Read.Length;
+
+	PVOID sysBuff = pIrp->AssociatedIrp.SystemBuffer;
+	if (!MmIsAddressValid(sysBuff) && stack > 0) {
+		DbgPrint("sysBuff is null！\n");
+		pIrp->IoStatus.Information = 0;
+		pIrp->IoStatus.Status = STATUS_SUCCESS;
+		IoCompleteRequest(pIrp, IO_NO_INCREMENT);
+		return STATUS_SUCCESS;
+	}
+	DbgPrint("sysBuff is %p！\n", sysBuff);
+	pIrp->IoStatus.Information = sizeof(stack);
+	pIrp->IoStatus.Status = STATUS_SUCCESS;
+	return STATUS_SUCCESS;
+
+}
+
+
+
+
+EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegisterPath) {
+	RegisterPath;
+	DriverObject->DriverUnload = DriverUnload;
+
+	NTSTATUS status;
+	PDEVICE_OBJECT PDeviceObj;
+	UNICODE_STRING DeviceName = RTL_CONSTANT_STRING(L"\\Device\\DemoInject");
+	UNICODE_STRING symbolLINKName = RTL_CONSTANT_STRING(L"\\??\\DemoInject");
+
+	//创建设备对象
+	status = IoCreateDevice(DriverObject, NULL, &DeviceName, FILE_DEVICE_UNKNOWN, NULL, FALSE, &PDeviceObj);
+	if (!NT_SUCCESS(status)){
+		return status ;
+	}
+	
+	PDeviceObj->Flags |= DO_BUFFERED_IO ; // 将 PDeviceObj->Flags 设置为 0，意味着清除所有的标志位，将设备对象恢复到默认状态。
+
+	//创建 R0 与 R3 的符号链接 
+	status = IoCreateSymbolicLink(&symbolLINKName, &DeviceName);
+	if (!NT_SUCCESS(status)) {
+		return status;
+	}
+	//例程
+	DbgPrint("驱动加载成功！\n");
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = DispatchCreate;
+	DbgPrint("驱动加载成功！\n");
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = DispatchClose;
+	DriverObject->MajorFunction[IRP_MJ_READ] = DispatchRead;
+	DriverObject->MajorFunction[IRP_MJ_WRITE] = DispatchWrite;
+	DbgPrint("驱动加载成功！\n");
+	return STATUS_SUCCESS;
+}
