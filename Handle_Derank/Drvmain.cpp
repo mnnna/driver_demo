cf86a871ca419c8238639e081704c80c2768b3a2
zhengyuhang <850400628@qq.com>
2024-09-18 03:08:21 +0800

    update

--- Handle_Derank/Drvmain.cpp
+++ Handle_Derank/Drvmain.cpp
@@ -1,53 +1,61 @@
 #include <ntifs.h>
-#include<ntddk.h>
+#include <ntddk.h>
 
-PVOID g_callBack_Handle = 0; 
-HANDLE g_pid = 0;
 
-OB_PREOP_CALLBACK_STATUS PobPreOperationCallback( PVOID RegistrationContext,POB_PRE_OPERATION_INFORMATION OperationInformation) {
-	UNREFERENCED_PARAMETER(RegistrationContext);
-	PEPROCESS process = { 0 };
-	PsLookupProcessByProcessId(g_pid,&process); 
+PVOID g_callBack_Handle = 0;
+HANDLE g_pid = (HANDLE)4236;
 
-	if (OperationInformation->Operation == OB_OPERATION_HANDLE_CREATE) {
-		if (OperationInformation->Object == process) {
-			OperationInformation->Parameters->CreateHandleInformation.DesiredAccess = 0;
-		}
-	}
-
-	return OB_PREOP_SUCCESS;
-}
-
-VOID DriverUnload(PDRIVER_OBJECT DriverObject) {
-	UNREFERENCED_PARAMETER(DriverObject);
+void Unload(PDRIVER_OBJECT DriverObjec) {
+	UNREFERENCED_PARAMETER(DriverObjec);
 	if (g_callBack_Handle != 0) {
 		ObUnRegisterCallbacks(g_callBack_Handle);
 	}
+
 }
 
+OB_PREOP_CALLBACK_STATUS ObProceeCallBack(PVOID RegistrationContext, POB_PRE_OPERATION_INFORMATION OperationInformation) {
+	UNREFERENCED_PARAMETER(RegistrationContext);
+	PEPROCESS process{ 0 };
+	PsLookupProcessByProcessId(g_pid, &process);
+	if (OperationInformation->Operation == OB_OPERATION_HANDLE_CREATE) {
+		if (OperationInformation->Object == process) {
 
-EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, UNICODE_STRING RegisterPath) {
+			OperationInformation->Parameters->CreateHandleInformation.DesiredAccess = 0;
+		}
+	}
+	return OB_PREOP_SUCCESS;
+
+}
+
+EXTERN_C NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegisterPath) {
+	ULONG_PTR pDrvSection = (ULONG_PTR)DriverObject->DriverSection;
+	*(PULONG)(pDrvSection + 0x68) |= 0x20; /* From https ://blog.csdn.net/qq_41252520/article/details/134386049 */
+
 	UNREFERENCED_PARAMETER(RegisterPath);
-	
 
-	OB_CALLBACK_REGISTRATION callback = { 0 };
-	OB_OPERATION_REGISTRATION callbackOper = { 0 };
+	OB_CALLBACK_REGISTRATION callBackReg{ 0 };
+	OB_OPERATION_REGISTRATION callbackOper{ 0 };
 
 
-	callback.Version = OB_FLT_REGISTRATION_VERSION;
-	callback.OperationRegistrationCount = 1;
-	callback.Altitude = RTL_CONSTANT_STRING(L"123.321123");
-	callback.RegistrationContext = nullptr;
-	callback.OperationRegistration = &callbackOper;
+	callBackReg.Version = OB_FLT_REGISTRATION_VERSION;
+	callBackReg.OperationRegistrationCount = 1;
+	callBackReg.Altitude = RTL_CONSTANT_STRING(L"371055.1351");
+	callBackReg.RegistrationContext = nullptr;
+	callBackReg.OperationRegistration = &callbackOper;
 
 	callbackOper.ObjectType = PsProcessType;
 	callbackOper.Operations = OB_OPERATION_HANDLE_CREATE | OB_OPERATION_HANDLE_DUPLICATE;
-	callbackOper.PreOperation = &PobPreOperationCallback;
+	callbackOper.PreOperation = ObProceeCallBack;
 	callbackOper.PostOperation = nullptr;
 
-	DriverObject->DriverUnload = DriverUnload;
+	/*UNICODE_STRING DestinationString;
+	RtlInitUnicodeString(&DestinationString, L"371055.1351")*/
 
-	return ObRegisterCallbacks(&callback, &g_callBack_Handle);
 
+	DriverObject->DriverUnload = Unload;
+
+
+
+
+	return ObRegisterCallbacks(&callBackReg, &g_callBack_Handle);
 }
-
