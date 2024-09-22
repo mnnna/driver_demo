de23776271ea58da89f25d232534183e38e3d597
zhengyuhang <850400628@qq.com>
2024-09-22 20:00:38 +0800

    update

--- DrvInject/HideMemory.cpp
+++ DrvInject/HideMemory.cpp
@@ -19,19 +19,57 @@ struct _MMPTE
 //0x30 bytes (sizeof)
 struct _MMPFN
 {
-	void* padding1;
+	void* padding1; 
 	void* pte_address;
 	struct _MMPTE OriginalPte;                                      //0x10
 	char padding2[0x18];															//0x28
 };
 
-_MMPFN get_MemPfnDataBase() {
-	
+uint64_t va_to_pa(void* va) {
+	return MmGetPhysicalAddress(va).QuadPart;
+}
+
+_MMPFN* get_MemPfnDataBase() {
+	UNICODE_STRING func_name{ 0 };
+	RtlInitUnicodeString(&func_name, L"MmGetVirtualForPhysical");
+
+	auto start = (unsigned char*)MmGetSystemRoutineAddress(&func_name);
+
+	hde64s hde64 = { 0 };
+	int index = 0;
+	while (hde64_disasm(&start[index], &hde64)) {
+		if (hde64.len == 10) break; 
+		index += hde64.len; 
+	}
+
+	auto tmp = *(PULONG64)(start[index + 2]);
+	return (_MMPFN*)(tmp - 8);
 }
 
 bool hide_mem(HANDLE pid, void* va, ULONG attribute)
-{
+{	
+	NTSTATUS status; 
+	KAPC_STATE apc = { 0 };
+	PEPROCESS process = { 0 };
+	status = PsLookupProcessByProcessId(pid, &process);
+	if (!NT_SUCCESS(status)) {
+		
+		DbgPrint("Failed to get process");
+		return false; 
+	}
 
+	KeStackAttachProcess(process, &apc);
 
-	return false;
+	void* align_va = PAGE_ALIGN(va);
+	uint64_t pa =  va_to_pa(align_va);
+	uint64_t pfn = pa >> 12;
+
+	auto MmPfnDataBase = get_MemPfnDataBase();
+
+	auto mmpfn = &MmPfnDataBase[pfn]; // 索引是 PFN ，物理地址的叶帧编号
+	mmpfn->OriginalPte.Protection = attribute;
+
+	ObDereferenceObject(process);
+	KeUnstackDetachProcess(&apc);
+	return true; 
 }
--- DrvInject/HideMemory.h
+++ DrvInject/HideMemory.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <ntddk.h>
-
+#include "hde/hde64.h"
 
 extern bool hide_mem(HANDLE pid, void* va, ULONG attribute);
