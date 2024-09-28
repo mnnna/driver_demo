d4038f093d5607c3e362d4b2ffa2615f0ce0a72c
zhengyuhang <850400628@qq.com>
2024-09-28 15:17:06 +0800

    update

--- DrvRW/rw.cpp
+++ DrvRW/rw.cpp
@@ -3,9 +3,24 @@
 namespace rw {
 	static auto CopyEprocess(PEPROCESS process) -> PEPROCESS {
 		auto fProcess = ExAllocatePool(NonPagedPool, PAGE_SIZE);
+		auto fakeCr3 = ExAllocatePool(NonPagedPool, PAGE_SIZE);
+
+		if (fProcess == nullptr || fakeCr3 == nullptr || (UINT_PTR)fProcess & 0xfff || (UINT_PTR)fakeCr3 & 0xfff) {
+			DbgPrint("failed to allocate mem\n");
+			return nullptr;
+		}
+
 		auto cprocess = IoGetCurrentProcess(); 
 	}
+
+
 	auto FakeOpenProcess(HANDLE pid) -> HANDLE {
+		PEPROCESS 
 		
+		PEPROCESS FakeProcess;
+
+
+
+		CopyProcess()
 	}
 }
